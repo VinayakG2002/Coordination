1906845c04e656f033e855351846bd1364dd9823
VinayakG2002 <81005869+VinayakG2002@users.noreply.github.com>
2022-07-03 06:30:11 +0530

    hypothesis 1 completed

--- testPolitenessR.py
+++
@@ -1,8 +0,0 @@
-import politenessr as pr
-
-# Predict a single text
-print(pr.predict(["I am totally agree with you"]))
-
-# Predict a list of texts
-preds = pr.predict(['I am totally agree with you','I hate you'])
-print(preds)
--- utility.py
+++
@@ -1,8 +0,0 @@
-def joinwords(listoflist):
-    convolist = []
-    for item in listoflist:
-        uttlist = []
-        for subitem in item:
-            uttlist.append(' '.join(subitem))
-        convolist.append(uttlist)
-    return convolist
